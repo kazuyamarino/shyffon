ffe1c9d88ec1d9b61b0d016c5ec8045155eb570b
kazuyamarino <vikry.yuansah@gmail.com>
2023-06-16 13:41:06 +0700

    update cli funct & index.php

--- .cli/nsy.core.sh
+++ .cli/nsy.core.sh
@@ -13,11 +13,13 @@
 . .cli/mk.migration.sh
 . .cli/mk.model.sh
 . .cli/mk.module.sh
+. .cli/mk.route.sh
 . .cli/mk.middleware.sh
+. .cli/run.migration.sh
 
 if [ -z $1 ]; then
 	printf "Command does not exist or undefined\n"
-	printf "It should be like this 'nsy <command>'\n"
+	printf "It should be like this 'nsy [command]'\n"
 else
 	if [ $1 = "show:module" ]; then
 		show_module
@@ -25,7 +27,7 @@ else
 		show_controller $2 $3
 	elif [ $1 = "show:model" ]; then
 		show_model $2 $3
-	elif [ $1 = "show:migration" ]; then
+	elif [ $1 = "show:migrate" ]; then
 		show_migration
 	elif [ $1 = "--install" ]; then
 		run_install
@@ -43,15 +45,19 @@ else
 		make_controller $2 $3 $4
 	elif [ $1 = "make:model" ]; then
 		make_model $2 $3 $4
-	elif [ $1 = "make:migration" ]; then
+	elif [ $1 = "make:migrate" ]; then
 		make_migration $2
 	elif [ $1 = "make:module" ]; then
 		make_module $2
+	elif [ $1 = "make:route" ]; then
+		make_route $2
 	elif [ $1 = "make:after-middleware" ]; then
 		make_after_middleware $2
 	elif [ $1 = "make:before-middleware" ]; then
 		make_before_middleware $2
+	elif [ $1 = "run:migrate" ]; then
+		run_migration $2
 	else
-	    printf "NSY Command $1 : not found\n"
+		printf "NSY Command $1 : not found\n"
 	fi
 fi
---
+++ .cli/run.migration.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+run_migration() {
+	if [ -z $1 ]
+	then
+		printf "Migration [command options] undefined\n"
+		printf "It should be like this 'run:migration [command options]'\n"
+	elif [ -n $1 ]
+	then
+		entry=$1
+
+		project_name=$(basename $PWD)
+		hs_name=$(hostname -i)
+		
+		case $entry in
+			"all")
+				search_dir=./System/Migrations/*.php
+				for entry in $search_dir
+				do
+					mig_name=$(basename $entry)
+
+					wget -q -O MIGRATIONLOG http://$hs_name/$project_name/migdown=${mig_name%.*}
+					wget -q -O MIGRATIONLOG http://$hs_name/$project_name/migup=${mig_name%.*}
+
+					echo "Successfully migrating down "${mig_name%.*}
+					echo "Successfully migrating up "${mig_name%.*}
+				done
+
+				printf "\nAll migration classes run successfully\n"
+				printf "Please see the results in the 'Database'\n"
+			;;
+			"list")
+				search_dir=./System/Migrations/*.php
+				arr_mig_name=()
+				for entry in $search_dir
+				do
+					mig_name=($(basename $entry))
+					arr_mig_name+=("$mig_name")
+				done
+
+				for i in "${!arr_mig_name[@]}"; do
+					printf "%s) %s\n" $(($i + 1)) "${arr_mig_name[$i]}"
+				done
+				printf 'Select a migration class from the above list: '
+				IFS= read -r opt
+				if (( (opt > 0) && (opt <= "${#arr_mig_name[@]}") )); then
+					
+					wget -q -O MIGRATIONLOG http://$hs_name/$project_name/migdown=${arr_mig_name[$((opt - 1))]%.*}
+					wget -q -O MIGRATIONLOG http://$hs_name/$project_name/migup=${arr_mig_name[$((opt - 1))]%.*}
+
+					printf "\n"
+					echo "Successfully migrating down "${arr_mig_name[$((opt - 1))]%.*}
+					echo "Successfully migrating up "${arr_mig_name[$((opt - 1))]%.*}
+
+					printf "\nMigration class run successfully\n"
+					printf "Please see the results in the 'Database'\n"
+				else
+					printf "\nMigration class not selected or not found\n"
+				fi
+			;;
+			*)
+				if [ ! -e ./System/Migrations/"$entry.php" ]
+				then
+					printf "Migration warning\n"
+					printf "No Migration found\n"
+				else
+					mig_name=$(basename $entry)
+
+					wget -q -O MIGRATIONLOG http://$hs_name/$project_name/migdown=${mig_name%.*}
+					wget -q -O MIGRATIONLOG http://$hs_name/$project_name/migup=${mig_name%.*}
+
+					echo "Successfully migrating down "${mig_name%.*}
+					echo "Successfully migrating up "${mig_name%.*}
+
+					printf "\nMigration class run successfully\n"
+					printf "Please see the results in the 'Database'\n"
+				fi
+			;;
+		esac
+	fi
+}

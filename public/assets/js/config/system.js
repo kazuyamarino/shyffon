bdcb4759f2fd8dbbbe68bc1b105559f28d1877c4
kazuyamarino <vikry.yuansah@gmail.com>
2024-06-12 10:52:49 +0700

    minor fix

--- public/assets/js/config/system.js
+++ public/assets/js/config/system.js
@@ -10,34 +10,40 @@
 var dirname = "shyffon"; // defined
 // var dirname = ""; // undefined
 
-function base_url(url)
+function base_url(url, port)
 {
+	var colon = ":";
 	var slash = "/";
 	var slashes ="//";
 	var protocol = location.protocol;
 	var host = window.location.hostname;
 
 	// if dirname not empty or defined
-	if (dirname ) {
+	if (dirname) {
 		// then show this base_url with dirname + slash result
-		var base_url = protocol + slashes + host + slash + dirname + slash;
-		if (url ) {
+		if (port) {
+			var base_url = protocol + slashes + host + colon + port + slash + dirname + slash;
+		} else {
+			var base_url = protocol + slashes + host + slash + dirname + slash;
+		}
+
+		if (url) {
 			return base_url + url;
 		} else {
 			return base_url;
 		}
 	} else {
 		// else show base_url without dirname + slash result
-		var base_url = protocol + slashes + host + slash;
-		if (url ) {
+		if (port) {
+			var base_url = protocol + slashes + host + colon + port + slash;
+		} else {
+			var base_url = protocol + slashes + host + slash;
+		}
+		
+		if (url) {
 			return base_url + url;
 		} else {
 			return base_url;
 		}
 	}
 }
-
-/*
-Foundation initialize
- */
-$(document).foundation();

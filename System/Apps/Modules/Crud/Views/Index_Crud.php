dd3631cc15c63c32a45497a428b2518bf6616f45
kazuyamarino <vikry.yuansah@gmail.com>
2024-06-12 14:29:33 +0700

    minor fix

--- System/Apps/Modules/Crud/Views/Index_Crud.php
+++ System/Apps/Modules/Crud/Views/Index_Crud.php
@@ -81,18 +81,19 @@
 						<button id="multidelete-btn" class="button alert" type="button">Delete Selected</button>
 					</div>
 				</div>
-				<table id="example" class="display responsive" cellspacing="0" width="100%">
+				<table id="example_crud" class="display responsive" cellspacing="0" width="100%">
 					<thead>
 						<tr>
-							<th></th>
-							<th>ID</th>
-							<th>User Code</th>
-							<th>User Name</th>
-							<th>Status</th>
-							<th>Create Date</th>
-							<th>Update Date</th>
-							<th>Additional Date</th>
-							<th>Action</th>
+							<th class="id">
+								<input type="checkbox" id="select-all">
+							</th>
+							<th class="user_code">User Code</th>
+							<th class="user_name">User Name</th>
+							<th class="user_status">User Status</th>
+							<th class="create_date">Create Date</th>
+							<th class="update_date">Update Date</th>
+							<th class="additional_date">Additional Date</th>
+							<th class="action">Action</th>
 						</tr>
 					</thead>
 				</table>
--- public/assets/js/datatables/init_crud.js
+++ public/assets/js/datatables/init_crud.js
@@ -1,90 +1,105 @@
 $(document).ready(function () {
-	var table = $("#example").DataTable({
-		"sPaginationType": "full_numbers",
-		"iDisplayLength": 5,
-		"aLengthMenu": [[5, 10, 25, 50, -1], [5, 10, 25, 50, "All"]],
-		"stateSave": true,
-		"ajax": {
-			"url": base_url("crud/data.json") // base_url can be managed in system.js located in the assets/js/config directory
-		},
-		"columns": [
-			{ "data": "id" },
-			{ "data": "id" },
-			{ "data": "user_code" },
-			{ "data": "user_name" },
-			{ "data": "user_status" },
-			{ "data": "create_date" },
-			{ "data": "update_date" },
-			{ "data": "additional_date" },
-			{ "data": "id" }
+	var table = $("#example_crud").DataTable({
+		sPaginationType: "full_numbers",
+		iDisplayLength: 5,
+		aLengthMenu: [
+			[5, 10, 25, 50, -1],
+			[5, 10, 25, 50, "All"],
 		],
-		'columnDefs': [
+		stateSave: true,
+		ajax: {
+			url: base_url("crud/data.json"), // base_url can be managed in system.js located in the assets/js/config directory
+		},
+		columns: [
+			{ data: "id", orderable: false },
+			{ data: "user_code" },
+			{ data: "user_name" },
+			{ data: "user_status" },
+			{ data: "create_date" },
+			{ data: "update_date" },
+			{ data: "additional_date" },
+			{ data: "id" },
+		],
+		columnDefs: [
 			{
-				'targets': 0,
-				'checkboxes': {
-					'selectRow': true
+				targets: "id",
+				orderable: false,
+				className: "select-checkbox",
+				render: function (data, type, full, meta) {
+					return `<div class="text-center"><input name="admin_id[]" type="checkbox" value="${data}"></div>`;
+				},
+			},
+			{
+				targets: ["user_status"],
+				width: "8%",
+				render: function (data, type, full, meta) {
+					return `<div class="text-center">${data}</div>`;
 				}
 			},
 			{
-				"targets": [4],
-				"className": "text-center",
-				"width": "8%"
+				targets: ["user_code", "user_name", "create_date", "update_date", "additional_date"],
+				render: function (data, type, full, meta) {
+					return `<div class="text-center">${data}</div>`;
+				}
 			},
 			{
-				"targets": [1, 2, 5, 6, 7],
-				"className": "text-center"
-			},
-			{
-				"targets": [8],
-				"className": "text-center",
-				"render": function (data, type, full, meta) {
+				targets: ["action"],
+				render: function (data, type, full, meta) {
 					if (data) {
-						return '<div class="action-icon">' +
-							'<a id="fetch-btn" href="' + base_url('crud/fetch/' + data) + '"><i class="far fa-edit"></i></a>' +
-							'<a id="delete-btn" data-open="delete-modal" data-url="' + base_url('crud/delete/' + data) + '"><i class="far fa-trash-alt"></i></a>' +
-							'</div>';
+						return `<div class="action-icon">
+							<a id="fetch-btn" href="${base_url("crud/fetch/" + data)}"><i class="far fa-edit"></i></a>
+							<a id="delete-btn" data-open="delete-modal" data-url="${base_url("crud/delete/" + data)}"><i class="far fa-trash-alt"></i></a>
+						</div>`;
 					} else {
-						return '';
+						return "";
 					}
-				}
-			}
+				},
+			},
 		],
-		'select': {
-			'style': 'multi'
+		select: {
+			style: "multi",
+			selector: "td:first-child input[type='checkbox']",
 		},
-		"oLanguage": {
-			"oPaginate": {
-				"sFirst": "<i class='fas fa-angle-double-left'></i>",
-				"sLast": "<i class='fas fa-angle-double-right'></i>",
-				"sPrevious": "<i class='fas fa-angle-left'></i>",
-				"sNext": "<i class='fas fa-angle-right'></i>"
+		order: [[1, "asc"]],
+		oLanguage: {
+			oPaginate: {
+				sFirst: "<i class='fas fa-angle-double-left'></i>",
+				sLast: "<i class='fas fa-angle-double-right'></i>",
+				sPrevious: "<i class='fas fa-angle-left'></i>",
+				sNext: "<i class='fas fa-angle-right'></i>",
+			},
+		},
+	});
+
+	// Handle click on "Select all" control
+	$("#select-all").on("click", function () {
+		var rows = table.rows({ search: "applied" }).nodes();
+		$('input[type="checkbox"]', rows).prop("checked", this.checked);
+	});
+
+	// Handle click on checkbox to set state of "Select all" control
+	$("#example_crud tbody").on("change", 'input[type="checkbox"]', function () {
+		if (!this.checked) {
+			var el = $("#select-all").get(0);
+			if (el && el.checked && "indeterminate" in el) {
+				el.indeterminate = true;
 			}
 		}
 	});
 
 	// Handle reset button on Datatables
 	$("#reset-filter").on("click", function () {
-		// reset Datatables filter
-		table.search('').columns().search('').draw();
-
-		// clear Datatables state
+		table.search("").columns().search("").draw();
 		table.state.clear();
 	});
 
 	// Handle form submission event
-	$('#multidelete-frm').on('submit', function (e) {
+	$("#multidelete-frm").on("submit", function (e) {
 		var form = this;
-
 		var rows_selected = table.column(0).checkboxes.selected();
-
-		// Iterate over all selected checkboxes
 		$.each(rows_selected, function (index, rowId) {
-			// Create a hidden element
 			$(form).append(
-				$('<input>')
-					.attr('type', 'text')
-					.attr('name', 'admin_id[]')
-					.val(rowId)
+				$("<input>").attr("type", "text").attr("name", "admin_id[]").val(rowId)
 			);
 		});
 	});
